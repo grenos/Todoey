5c81b7401f66eccf4000b62a94fd216d7e56c841
mcsaatchidevelopers <tools@mcsaatchi-milano.com>
2019-09-25 14:34:29 +0200

    convert to MVC design pattern

--- Todoey/Controllers/TodoListVC.swift
+++ Todoey/Controllers/TodoListVC.swift
@@ -15,52 +15,64 @@ import UIKit
 class TodoListVC: UITableViewController {
     
     // sample items to use for now
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogorgon"]
+    var itemArray = [Item]()
     
     // An interface to the user’s defaults database,
     // where you store key-value pairs persistently across launches of your app.
     let defaults = UserDefaults.standard
 
+    
     /// viewDidLoad()
     //MARK: - viewDidLoad()
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem1 = Item()
+        newItem1.title = "Buy Eggos"
+        itemArray.append(newItem1)
+        
+        
         // we check if array exists in local storage
         // if yes we use it as the source to populate the cells of the tableView
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
     }
     /// /// ///
     
     
-    /// - TableView Delegate Methods (append cell to rows)
-    //MARK: - TableView Delegate Methods (append cell to rows)
+    
+    
+    /// - TableView Delegate Methods (tableView UI)
+    //MARK: - TableView Delegate Methods (tableView UI)
 
     //ui methods
     // do something when a row is tapped
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
+        // on cell tap toggles the done property of the item object
+        // if true then false ecc
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
         // removes the grey background from a selected row
         tableView.deselectRow(at: indexPath, animated: true)
         
-        // get current tapped row
-        let tappedRow = tableView.cellForRow(at: indexPath)
-        // check if has checkmark and add or remove acordingly
-        if tappedRow?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        // update tableView with latest "state" changes after the tap
+        tableView.reloadData()
+        
     }
     /// /// ///
     
     
     
     
-    /// - Tableview Datasource Methods (cell)
-    //MARK: - Tableview Datasource Methods (cell)
+    /// - Tableview Datasource Methods (append cell to rows)
+    //MARK: - Tableview Datasource Methods (append cell to rows)
     
     // set the number of rows in table
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -73,9 +85,15 @@ class TodoListVC: UITableViewController {
         //pass the name we gave it on the identifier editor
         //index path we give the current index passed in this function by the table
         let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        // indermediate let
+        // select each item of the array by passing the current index of the row
+        let item = itemArray[indexPath.row]
         //set the inner text of each cell
-        // selecting each item of the array by passing the current index of the row
-        cell.textLabel?.text = itemArray[indexPath.row]
+        // passing the title property of object
+        cell.textLabel?.text = item.title
+        // check if object has property done == true
+        // show or hide checkmark
+        cell.accessoryType = item.done ? .checkmark : .none
         //return the cell back to the tableView
         return cell
     }
@@ -100,7 +118,11 @@ class TodoListVC: UITableViewController {
         // this is a method to confirm the action (whatever we do with the alert)
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             // what will happen when user clicks the add item button on the alert
-            self.itemArray.append(textField.text!)
+            // we create a new Item object and we pass the value of the text input
+            // as the title property of the object, then we push it on out itemsArray
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
             // save our updated todo array in the local storage
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             // make tableView to re-print the screen with new data
