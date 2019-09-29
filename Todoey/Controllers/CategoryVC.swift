0871c2aa1583def4daf8cc0194d4c115bdf05ef4
grenos <Vasilis.green@gmail.com>
2019-09-29 20:07:41 +0200

    editor spaces alignment etc (no code changes)

--- Todoey/Controllers/CategoryVC.swift
+++ Todoey/Controllers/CategoryVC.swift
@@ -11,132 +11,131 @@ import CoreData
 
 class CategoryVC: UITableViewController {
     
-    // init a new array with type Category Class from CoreData
-    var CategoryArray = [Category]()
-    // get a reference of the context
-    let contextState = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+  // init a new array with type Category Class from CoreData
+  var CategoryArray = [Category]()
+  // get a reference of the context
+  let contextState = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        loadCategories()
-    }
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    loadCategories()
+  }
     
   
     
-    /// Tableview Datasource Methods
-    //MARK: - Tableview Datasource Methods
+  /// Tableview Datasource Methods
+  //MARK: - Tableview Datasource Methods
     
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // set rows of table to length of array
-        return CategoryArray.count
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // set rows of table to length of array
+    return CategoryArray.count
+  }
+    
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    // make cells reusable
+    let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+    // set cell's textlabel equal to what the user has entered as an input in the alert
+    cell.textLabel?.text = CategoryArray[indexPath.row].name
+    return cell
+  }
+  /// /// ///
+    
+    
+    
+    
+    
+  /// Tableview Delegate Methods
+  //MARK: - Tableview Delegate Methods
+    
+  // on tap of a cell/row
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    performSegue(withIdentifier: "goToItems", sender: self)
+  }
+    
+  // do somethig before the segue happens
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    // get the destination of the segue
+    let destinationVC = segue.destination as! TodoListVC
+    
+    // if we have selected a category
+    // so there IS an active index
+    if let activeIndex = tableView.indexPathForSelectedRow {
+      // set the value of selectedCategory declared on TodoListVC
+      // with the index of the tapped cell/row
+      destinationVC.selectedCategory = CategoryArray[activeIndex.row]
     }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // make cells reusable
-        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
-        // set cell's textlabel equal to what the user has entered as an input in the alert
-        cell.textLabel?.text = CategoryArray[indexPath.row].name
-        return cell
-    }
-    /// /// ///
-    
-    
-    
-    
-    
-    /// Tableview Delegate Methods
-    //MARK: - Tableview Delegate Methods
-    
-    // on tap of a cell/row
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        performSegue(withIdentifier: "goToItems", sender: self)
-    }
-    
-    // do somethig before the segue happens
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // get the destination of the segue
-        let destinationVC = segue.destination as! TodoListVC
-        
-        // if we have selected a category
-        // so there IS an active index
-        if let activeIndex = tableView.indexPathForSelectedRow {
-            // set the value of selectedCategory declared on TodoListVC
-            // with the index of the tapped cell/row
-            destinationVC.selectedCategory = CategoryArray[activeIndex.row]
-        }
-    }
-    /// /// ///
+  }
+  /// /// ///
     
     
     
     
 
-    /// TableView Manipulation Methods
-    //MARK: - TableView Manipulation Methods
-    func saveCategories() {
-        // save context
-        do {
-            try contextState.save()
-        } catch {
-            print("Error saving in context: \(error)")
-        }
-        // reload tableView to get new changes on screen
-        tableView.reloadData()
-        
+  /// TableView Manipulation Methods
+  //MARK: - TableView Manipulation Methods
+  func saveCategories() {
+    // save context
+    do {
+      try contextState.save()
     }
-    
-    func loadCategories() {
-        // declare the fetch request that we'll do on context
-        let request : NSFetchRequest<Category> = Category.fetchRequest()
-        
-        // make the fetch request with the above declared let as a query
-        do{
-            CategoryArray = try contextState.fetch(request)
-        } catch {
-            print("Error fetching from context: \(error)")
-        }
-        // reload tableView to get new changes on screen
-        tableView.reloadData()
+    catch {
+      print("Error saving in context: \(error)")
     }
-    /// /// ///
+      // reload tableView to get new changes on screen
+      tableView.reloadData()
+  }
     
+  func loadCategories() {
+    // declare the fetch request that we'll do on context
+    let request : NSFetchRequest<Category> = Category.fetchRequest()
     
-    
-    
-    
-    /// Add New Categories
-    //MARK - Add New Categories
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
-        
-        // init a new textfiled for the alert
-        var textFiled = UITextField()
-        // create the alert window
-        let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
-        // create the button that on click will do:
-        let action = UIAlertAction(title: "Add", style: .default) { (action) in
-            
-            // create an object from the Category Class from context
-            let newCategory = Category(context: self.contextState)
-            // set its name property equal to the user's input
-            newCategory.name = textFiled.text!
-            // push the object to our array
-            self.CategoryArray.append(newCategory)
-            // call the function that saves the array into context
-            self.saveCategories()
-
-        }
-        
-        // add the action to the alert window
-        alert.addAction(action)
-        // add the textfield from above in the allert
-        alert.addTextField { (field) in
-            textFiled = field
-            textFiled.placeholder = "Add a new category"
-        }
-        // call the alert to show up on screen
-        present(alert, animated: true, completion: nil)
+    // make the fetch request with the above declared let as a query
+    do{
+      CategoryArray = try contextState.fetch(request)
+    } catch {
+      print("Error fetching from context: \(error)")
     }
-    /// /// ///
+      // reload tableView to get new changes on screen
+      tableView.reloadData()
+  }
+  /// /// ///
+    
+    
+    
+    
+    
+  /// Add New Categories
+  //MARK - Add New Categories
+  @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    // init a new textfiled for the alert
+    var textFiled = UITextField()
+    // create the alert window
+    let alert = UIAlertController(title: "Add New Category", message: "", preferredStyle: .alert)
+    
+    // create the button that on click will do:
+    let action = UIAlertAction(title: "Add", style: .default) { (action) in
+      // create an object from the Category Class from context
+      let newCategory = Category(context: self.contextState)
+      // set its name property equal to the user's input
+      newCategory.name = textFiled.text!
+      // push the object to our array
+      self.CategoryArray.append(newCategory)
+      // call the function that saves the array into context
+      self.saveCategories()
+    }
+        
+    // add the action to the alert window
+    alert.addAction(action)
+    // add the textfield from above in the allert
+    alert.addTextField { (field) in
+      textFiled = field
+      textFiled.placeholder = "Add a new category"
+    }
+      // call the alert to show up on screen
+      present(alert, animated: true, completion: nil)
+  }
+  /// /// ///
     
 }
--- Todoey/Controllers/TodoListVC.swift
+++ Todoey/Controllers/TodoListVC.swift
@@ -14,209 +14,207 @@ import CoreData
 // because obviously our class is inheriting all of table view controller's attributes
 class TodoListVC: UITableViewController {
     
-    var itemArray = [Item]()
+  var itemArray = [Item]()
     
-    // call loadItems() only if selectedCategory has a value
-    // otherwise it will crash the app
-    var selectedCategory : Category? {
-        didSet{
-            loadItems()
-        }
+  // call loadItems() only if selectedCategory has a value
+  // otherwise it will crash the app
+  var selectedCategory : Category? {
+    didSet{
+      loadItems()
     }
+  }
     
     
-    // CORE DATA
-    // we grab a reference of the persistentContainer's context
-    // where we save the data (think redux store/state)
-    // CONTEXT is like an action/reducer -->
-    // pass the information of what we want to change -->
-    // to the database(persistentContainer)
-    let contextState = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+  // CORE DATA
+  // we grab a reference of the persistentContainer's context
+  // where we save the data (think redux store/state)
+  // CONTEXT is like an action/reducer -->
+  // pass the information of what we want to change -->
+  // to the database(persistentContainer)
+  let contextState = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     
     
 
-    /// viewDidLoad()
-    //MARK: - viewDidLoad()
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    
-    }
-    /// /// ///
-    
+  /// viewDidLoad()
+  //MARK: - viewDidLoad()
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  /// /// ///
     
     
     
     
-    /// - TableView Delegate Methods (tableView UI - onTap)
-    //MARK: - TableView Delegate Methods (tableView UI - onTap)
+    
+  /// - TableView Delegate Methods (tableView UI - onTap)
+  //MARK: - TableView Delegate Methods (tableView UI - onTap)
 
-    //ui methods
-    // do something when a row is tapped
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // on cell tap toggles the done property of the item object
-        // if true then false ecc
-        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+  //ui methods
+  // do something when a row is tapped
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    // on cell tap toggles the done property of the item object
+    // if true then false ecc
+    itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        ///UPDATE DATA
-        //itemArray[indexPath.row].setValue("Completed Maybe?", forKey: "Title")
+      ///UPDATE DATA
+      //itemArray[indexPath.row].setValue("Completed Maybe?", forKey: "Title")
         
-        ///DELETE DATA
-        //contextState.delete(itemArray[indexPath.row])
-        //itemArray.remove(at: indexPath.row)
+      ///DELETE DATA
+      //contextState.delete(itemArray[indexPath.row])
+      //itemArray.remove(at: indexPath.row)
         
-        // save the array to the plist file after
-        // we have made any changes to the done property of the object
-        saveItems()
-        // make tableView to re-print the screen with new data
-        tableView.reloadData()
-        // removes the grey background from a selected row
-        tableView.deselectRow(at: indexPath, animated: true)
-    }
-    /// /// ///
+      // save the array to the plist file after
+      // we have made any changes to the done property of the object
+      saveItems()
+      // make tableView to re-print the screen with new data
+      tableView.reloadData()
+      // removes the grey background from a selected row
+      tableView.deselectRow(at: indexPath, animated: true)
+  }
+  /// /// ///
     
     
     
     
     
-    /// - Tableview Datasource Methods (append cell to rows)
-    //MARK: - Tableview Datasource Methods (append cell to rows)
+  /// - Tableview Datasource Methods (append cell to rows)
+  //MARK: - Tableview Datasource Methods (append cell to rows)
     
-    // set the number of rows in table
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
-    }
+  // set the number of rows in table
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return itemArray.count
+  }
     
-    // set table cell for each row
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        //create a cell
-        //pass the name we gave it on the identifier editor
-        //index path we give the current index passed in this function by the table
-        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
-        // indermediate let
-        // select each item of the array by passing the current index of the row
-        let item = itemArray[indexPath.row]
-        //set the inner text of each cell
-        // passing the title property of object
-        cell.textLabel?.text = item.title
-        // check if object has property done == true
-        // show or hide checkmark
-        cell.accessoryType = item.done ? .checkmark : .none
-        //return the cell back to the tableView
-        return cell
-    }
-    /// /// ///
-    
-    
+  // set table cell for each row
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //create a cell
+    //pass the name we gave it on the identifier editor
+    //index path we give the current index passed in this function by the table
+    let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+    // indermediate let
+    // select each item of the array by passing the current index of the row
+    let item = itemArray[indexPath.row]
+    //set the inner text of each cell
+    // passing the title property of object
+    cell.textLabel?.text = item.title
+    // check if object has property done == true
+    // show or hide checkmark
+    cell.accessoryType = item.done ? .checkmark : .none
+    //return the cell back to the tableView
+    return cell
+  }
+  /// /// ///
     
     
     
-    /// - Add New Items (+alert)
-    //MARK: - Add New Items (+alert)
     
-    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+    
+  /// - Add New Items (+alert)
+  //MARK: - Add New Items (+alert)
+    
+  @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
-        //setup local var to save user's input inside
-        //and share with other methods in this function
-        var textField = UITextField()
+    //setup local var to save user's input inside
+    //and share with other methods in this function
+    var textField = UITextField()
         
-        // create an alert item
-        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+    // create an alert item
+    let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
         
-        // this is a method to confirm the action (whatever we do with the alert)
-        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            // what will happen when user clicks the add item button on the alert
+    // this is a method to confirm the action (whatever we do with the alert)
+    let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+      // what will happen when user clicks the add item button on the alert
             
-            // we grab contextState from the global var
-            // we init an object from the Item class to save out data in
-            let newItem = Item(context: self.contextState)
-            newItem.title = textField.text!
-            newItem.done = false
-            // we set the parent category with the index of the current category
-            // we have access to the parent category from the relationships we set on the dataModel
-            newItem.parentCategory = self.selectedCategory
-            self.itemArray.append(newItem)
-            // call method that creates and saves itemArray on the plist
-            self.saveItems()
+      // we grab contextState from the global var
+      // we init an object from the Item class to save out data in
+      let newItem = Item(context: self.contextState)
+      newItem.title = textField.text!
+      newItem.done = false
+      // we set the parent category with the index of the current category
+      // we have access to the parent category from the relationships we set on the dataModel
+      newItem.parentCategory = self.selectedCategory
+      self.itemArray.append(newItem)
+      // call method that creates and saves itemArray on the plist
+      self.saveItems()
             
-            // make tableView to re-print the screen with new data
-            self.tableView.reloadData()
-        }
-        
-        // add a textfield into the alert to be able to write new items
-        alert.addTextField { (alertTextFiled) in
-            alertTextFiled.placeholder = "Create New Item"
-            textField = alertTextFiled
-        }
-        
-        // add action in alerrt
-        alert.addAction(action)
-        // make alert apear on screen
-        present(alert, animated: true, completion: nil)
-        
+      // make tableView to re-print the screen with new data
+      self.tableView.reloadData()
     }
-    /// /// ///
-    
-    
-    
-    
-    
-    /// - Model Manipulation Methods (db calls)
-    //MARK: - Model Manipulation methods (db calls)
-    
-    func saveItems() {
-        do {
-            // save data to cntext (think store/state)
-            try contextState.save()
-        } catch {
-            print("Error saving context \(error)")
-        }
+        
+    // add a textfield into the alert to be able to write new items
+    alert.addTextField { (alertTextFiled) in
+      alertTextFiled.placeholder = "Create New Item"
+      textField = alertTextFiled
     }
+        
+    // add action in alerrt
+    alert.addAction(action)
+    // make alert apear on screen
+    present(alert, animated: true, completion: nil)
+        
+  }
+  /// /// ///
     
     
-    // we pass the request arg so we can re-use
-    // we also provide a default in case we call loadItems() but dont pass an arg
-    // like we do at the viewDidLoad() method
-    // we also pass a parameter for the predicate, we give a default of nil
-    // and also make it an optional
-    // because we call loadItems with diferent or no predicates at times
-    func loadItems(request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
-        // make a fetch request with a fetch call
-        // from context so we can read the data
-        // passing the rquest we created above
-        
-        // set a predicate to tell that every time we tap into a category
-        // we want to load only item created with the index of that category
-        // as a value for the parentCategory attribute
-        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
-        
-        // if predicate arg is passed set it inside the compoundPredicate
-        // with the categoryPredicate
-        if let additionalPredicate = predicate {
-            /// compoundPredicate
-            // something like Redux compose
-            // A specialized predicate that evaluates logical combinations of other predicates.
-            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
-        } else {
-            // else use only the categoryPredicate
-            request.predicate = categoryPredicate
-        }
-        
-        do {
-            // the fetch call return an array of objects
-            // that we then save in our itemsArray
-            itemArray = try contextState.fetch(request)
-        } catch {
-            print("Error requesting data \(error)")
-        }
-        
-        //re-print table view once we have all the fetched data
-        tableView.reloadData()
+    
+    
+    
+  /// - Model Manipulation Methods (db calls)
+  //MARK: - Model Manipulation methods (db calls)
+    
+  func saveItems() {
+    do {
+      // save data to cntext (think store/state)
+      try contextState.save()
+    } catch {
+      print("Error saving context \(error)")
     }
-    /// /// ///
+  }
     
     
-}
+  // we pass the request arg so we can re-use
+  // we also provide a default in case we call loadItems() but dont pass an arg
+  // like we do at the viewDidLoad() method
+  // we also pass a parameter for the predicate, we give a default of nil
+  // and also make it an optional
+  // because we call loadItems with diferent or no predicates at times
+  func loadItems(request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+    // make a fetch request with a fetch call
+    // from context so we can read the data
+    // passing the rquest we created above
+      
+    // set a predicate to tell that every time we tap into a category
+    // we want to load only item created with the index of that category
+    // as a value for the parentCategory attribute
+    let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+    // if predicate arg is passed set it inside the compoundPredicate
+    // with the categoryPredicate
+    if let additionalPredicate = predicate {
+      /// compoundPredicate
+      // something like Redux compose
+      // A specialized predicate that evaluates logical combinations of other predicates.
+      request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+      } else {
+        // else use only the categoryPredicate
+        request.predicate = categoryPredicate
+      }
+        
+      do {
+        // the fetch call return an array of objects
+        // that we then save in our itemsArray
+        itemArray = try contextState.fetch(request)
+      } catch {
+        print("Error requesting data \(error)")
+      }
+        
+      //re-print table view once we have all the fetched data
+      tableView.reloadData()
+  }
+  /// /// ///
+    
+} // class end
 
 
 
@@ -225,46 +223,44 @@ class TodoListVC: UITableViewController {
 //MARK: - Search bar methods
 extension TodoListVC: UISearchBarDelegate {
     
-    // method is going to be triggered once the user
-    // clicks the search button on the search bar
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        //query our database
-        let request: NSFetchRequest<Item> = Item.fetchRequest()
-        //we use the NSPredicate method to query our database
-        //format: title CONTAINS[cd] %@ --> https://academy.realm.io/posts/nspredicate-cheatsheet/
-        //arg: argument is the value we provide to search
-        let myPredicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
-        // set the predicate for the request as our predicate
-        request.predicate = myPredicate
-        // declare sortDescriptor method to sort our returned data
-        let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
-        // set the sortDescriptor for the request as our sortDescriptor
-        request.sortDescriptors = [sortDescriptor]
+  // method is going to be triggered once the user
+  // clicks the search button on the search bar
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    //query our database
+    let request: NSFetchRequest<Item> = Item.fetchRequest()
+    //we use the NSPredicate method to query our database
+    //format: title CONTAINS[cd] %@ --> https://academy.realm.io/posts/nspredicate-cheatsheet/
+    //arg: argument is the value we provide to search
+    let myPredicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+    // set the predicate for the request as our predicate
+    request.predicate = myPredicate
+    // declare sortDescriptor method to sort our returned data
+    let sortDescriptor = NSSortDescriptor(key: "title", ascending: true)
+    // set the sortDescriptor for the request as our sortDescriptor
+    request.sortDescriptors = [sortDescriptor]
         
-        // call function that makes the actual request
-        loadItems(request: request, predicate: myPredicate)
-    }
+    // call function that makes the actual request
+    loadItems(request: request, predicate: myPredicate)
+  }
     
     
-    // listen for changes in the text bar
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        // if searchBar is empty
-        if searchBar.text?.count == 0 {
-            // call load items without any specific param
-            // so it returns the entire db
-            loadItems()
+  // listen for changes in the text bar
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    // if searchBar is empty
+    if searchBar.text?.count == 0 {
+      // call load items without any specific param
+      // so it returns the entire db
+      loadItems()
             
-            /// DISPATCH MANAGER
-            // assigns the tasks and proccesses to diferent threads
-            // main == main thread (where all UI tasks should be)
-            DispatchQueue.main.async {
-                // put searchbar out of focus i.e. -->
-                // close the keyboard unFocus search bar
-                searchBar.resignFirstResponder()
-            }
-        }
+      /// DISPATCH MANAGER
+      // assigns the tasks and proccesses to diferent threads
+      // main == main thread (where all UI tasks should be)
+      DispatchQueue.main.async {
+      // put searchbar out of focus i.e. -->
+      // close the keyboard unFocus search bar
+      searchBar.resignFirstResponder()
+      }
     }
-    
-    
-    
-}
+  }
+        
+} // Extensions end
